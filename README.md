972d48e0668614109ba7324993bd1e7dbe53ee1e
Matthew McCartney <25149329+mattmccartneyuk@users.noreply.github.com>
2024-04-21 23:41:59 +0100

    Adding environment variable setup for Vultr API Key

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # scavenger
 A Windows Application for the automated provisioning of a Vultr VPS using an Ansible Playbook.
+
+## Set API Key
+In Scavenger.UI you will find a Example.Settings.settings file. This file should be renamed to Settings.settings, as it will not be tracked by version control. You can add your own Vultr API Key to the value of APIKEY. I recommend using Visual Studio's designer view for this action.
--- Scavenger.UI/MainWindow.xaml.cs
+++ Scavenger.UI/MainWindow.xaml.cs
@@ -1,15 +1,6 @@
-﻿using System.Net.Mime;
-using System.Text;
+﻿using Scavenger.Core;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Scavenger.Core;
 
 namespace Scavenger.UI
 {
@@ -27,6 +18,8 @@ namespace Scavenger.UI
             PopulateComboBox();
 
             _continentToCountryToCity = new ContinentToCountryToCity();
+
+            TextBlock.Text = Settings.Default.APIKEY;
         }
 
 
